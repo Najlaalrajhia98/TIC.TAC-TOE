f14e4d21786bf712e7e850225e05962eae92f0c8
Najlaalrajhia98 <najla.alrajhi@tra.gov.om>
2022-12-11 09:49:08 +0400

    added files

---
+++ src/TICTAC.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author LAP-7
+ *
+ */
+public class TICTAC {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
